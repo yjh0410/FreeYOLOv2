a5be6de5ff41d5bb3a5f8a521a2f8eaaab1fdfb7
yjh0410 <1394571815@qq.com>
2023-02-13 10:06:01 +0800

    train FreeYOLOv2-Tiny from the scratch on COCO with bs=16

--- train.sh
+++ train.sh
@@ -1,30 +1,30 @@
-# # Train FreeYOLO
-# python train.py \
-#         --cuda \
-#         -d coco \
-#         --root /mnt/share/ssd2/dataset/ \
-#         -v yolo_free_v2_tiny \
-#         -bs 16 \
-#         --max_epoch 300 \
-#         --wp_epoch 1 \
-#         --eval_epoch 10 \
-#         --ema \
-#         --fp16 \
-#         # --resume weights/coco/yolo_free_medium/yolo_free_medium_epoch_31_39.46.pth \
-#         # --pretrained weights/coco/yolo_free_medium/yolo_free_medium_39.46.pth \
-
-
-# Debug FreeYOLO on VOC
+# Train FreeYOLO
 python train.py \
         --cuda \
-        -d voc \
+        -d coco \
         --root /mnt/share/ssd2/dataset/ \
         -v yolo_free_v2_tiny \
         -bs 16 \
-        --max_epoch 25 \
+        --max_epoch 300 \
         --wp_epoch 1 \
-        --eval_epoch 5 \
+        --eval_epoch 10 \
         --ema \
         --fp16 \
         # --resume weights/coco/yolo_free_medium/yolo_free_medium_epoch_31_39.46.pth \
         # --pretrained weights/coco/yolo_free_medium/yolo_free_medium_39.46.pth \
+
+
+# # Debug FreeYOLO on VOC
+# python train.py \
+#         --cuda \
+#         -d voc \
+#         --root /mnt/share/ssd2/dataset/ \
+#         -v yolo_free_v2_tiny \
+#         -bs 16 \
+#         --max_epoch 25 \
+#         --wp_epoch 1 \
+#         --eval_epoch 5 \
+#         --ema \
+#         --fp16 \
+#         # --resume weights/coco/yolo_free_medium/yolo_free_medium_epoch_31_39.46.pth \
+#         # --pretrained weights/coco/yolo_free_medium/yolo_free_medium_39.46.pth \
