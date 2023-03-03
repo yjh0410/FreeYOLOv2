9bcb2344c355e43a99a28bf294015d72e089a97a
yjh0410 <1394571815@qq.com>
2023-03-03 13:18:17 +0800

    train FreeYOLOvx-Pico on COCO

--- train.sh
+++ train.sh
@@ -3,7 +3,7 @@ python train.py \
         --cuda \
         -d coco \
         --root /mnt/share/ssd2/dataset/ \
-        -v yolo_free_vx_tiny \
+        -v yolo_free_vx_pico \
         -bs 16 \
         --wp_epoch 3 \
         --max_epoch 300 \
