6dce134ba7926b98b33932961d66b34528fbb0dd
yjh0410 <1394571815@qq.com>
2023-03-14 19:32:24 +0800

    debug freeyolov2_nano

--- utils/solver/optimizer.py
+++ utils/solver/optimizer.py
@@ -9,17 +9,15 @@ def build_optimizer(cfg, model, base_lr=0.01, resume=None):
     print('--momentum: {}'.format(cfg['momentum']))
     print('--weight_decay: {}'.format(cfg['weight_decay']))
 
-    # YOLOv5 3-param group optimizer: 0) weights with decay, 1) weights no decay, 2) biases no decay
     g = [], [], []  # optimizer parameter groups
     bn = tuple(v for k, v in nn.__dict__.items() if 'Norm' in k)  # normalization layers, i.e. BatchNorm2d()
     for v in model.modules():
-        for p_name, p in v.named_parameters(recurse=0):
-            if p_name == 'bias':  # bias (no decay)
-                g[2].append(p)
-            elif p_name == 'weight' and isinstance(v, bn):  # weight (no decay)
-                g[1].append(p)
-            else:
-                g[0].append(p)  # weight (with decay)
+        if hasattr(v, 'bias') and isinstance(v.bias, nn.Parameter):  # bias (no decay)
+            g[2].append(v.bias)
+        if isinstance(v, bn):  # weight (no decay)
+            g[1].append(v.weight)
+        elif hasattr(v, 'weight') and isinstance(v.weight, nn.Parameter):  # weight (with decay)
+            g[0].append(v.weight)
 
     if cfg['optimizer'] == 'adam':
         optimizer = torch.optim.Adam(g[2], lr=base_lr, betas=(cfg['momentum'], 0.999))  # adjust beta1 to momentum
