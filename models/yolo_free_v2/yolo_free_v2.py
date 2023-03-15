ae773674bbfadea7749a88f954ae4ad73b76ce2e
yjh0410 <1394571815@qq.com>
2023-03-15 09:15:25 +0800

    debug rescale bbox

--- models/yolo_free_v2/yolo_free_v2.py
+++ models/yolo_free_v2/yolo_free_v2.py
@@ -105,7 +105,7 @@ class FreeYOLOv2(nn.Module):
         fmp_h, fmp_w = fmp_size
         anchor_y, anchor_x = torch.meshgrid([torch.arange(fmp_h), torch.arange(fmp_w)])
         # [H, W, 2] -> [HW, 2]
-        anchor_xy = torch.stack([anchor_x, anchor_y], dim=-1).float().view(-1, 2)
+        anchor_xy = torch.stack([anchor_x, anchor_y], dim=-1).float().view(-1, 2) + 0.5
         anchor_xy *= self.stride[level]
         anchors = anchor_xy.to(self.device)
 
