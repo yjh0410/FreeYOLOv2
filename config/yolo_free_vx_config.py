a583e61c9ca0f52998fcbb0509814de2483e83c2
yjh0410 <1394571815@qq.com>
2023-03-15 16:31:41 +0800

    add FreeYOLOvx-Pico

--- config/yolo_free_vx_config.py
+++ config/yolo_free_vx_config.py
@@ -3,6 +3,75 @@
 
 yolo_free_vx_config = {
     # P5
+    'yolo_free_vx_pico': {
+        # input
+        'mosaic_prob': 0.5,
+        'mixup_prob': 0.0,
+        'format': 'RGB',
+        'trans_config': {'degrees': 0.0,
+                          'translate': 0.1,
+                          'scale': 0.5,
+                          'shear': 0.0,
+                          'perspective': 0.0,
+                          'hsv_h': 0.015,
+                          'hsv_s': 0.7,
+                          'hsv_v': 0.4
+                          },
+        # model
+        'backbone': 'elannet',
+        'pretrained': True,
+        'bk_act': 'lrelu',
+        'bk_norm': 'BN',
+        'bk_dpw': True,
+        'p6_feat': False,
+        'p7_feat': False,
+        'width': 0.25,
+        'depth': 0.34,
+        'stride': [8, 16, 32],  # P3, P4, P5
+        # neck
+        'neck': 'sppf',
+        'expand_ratio': 0.5,
+        'pooling_size': 5,
+        'neck_act': 'lrelu',
+        'neck_norm': 'BN',
+        'neck_depthwise': True,
+        # fpn
+        'fpn': 'elan_pafpn',
+        'fpn_act': 'lrelu',
+        'fpn_norm': 'BN',
+        'fpn_depthwise': True,
+        # head
+        'head': 'decoupled_head',
+        'head_act': 'lrelu',
+        'head_norm': 'BN',
+        'num_cls_head': 2,
+        'num_reg_head': 2,
+        'head_depthwise': True,
+        # matcher
+        'matcher': {'center_sampling_radius': 2.5,
+                    'topk_candicate': 10},
+        # loss weight
+        'cls_loss': 'bce', # optional: bce, qfl, vfl
+        'loss_cls_weight': 1.0,
+        'loss_reg_weight': 5.0,
+        # training configuration
+        'no_aug_epoch': 20,
+        # optimizer
+        'optimizer': 'sgd',      # optional: sgd, adamw
+        'momentum': 0.937,         # SGD: 0.937;    AdamW: invalid
+        'weight_decay': 5e-4,      # SGD: 5e-4;     AdamW: 5e-2
+        'clip_grad': 10,           # SGD: 10.0;     AdamW: -1
+        # model EMA
+        'ema_decay': 0.9999,       # SGD: 0.9999;   AdamW: 0.9998
+        'ema_tau': 2000,
+        # lr schedule
+        'scheduler': 'linear',
+        'lr0': 0.01,              # SGD: 0.01;     AdamW: 0.004
+        'lrf': 0.01,               # SGD: 0.01;     AdamW: 0.05
+        'warmup_momentum': 0.8,
+        'warmup_bias_lr': 0.1,
+        },
+
     'yolo_free_vx_nano': {
         # input
         'mosaic_prob': 1.0,
