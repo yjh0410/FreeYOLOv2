b7e3e13a3b07e4028a5e5f02bcf9edaca8163b33
yjh0410 <1394571815@qq.com>
2023-03-15 10:03:19 +0800

    debug rescale bbox

--- models/yolo_free_v2/yolo_free_v2.py
+++ models/yolo_free_v2/yolo_free_v2.py
@@ -120,17 +120,6 @@ class FreeYOLOv2(nn.Module):
         Output:
             pred_box: (Tensor) [B, M, 4]
         """
-        if self.use_dfl:
-            B, M = pred_regs.shape[:2]
-            # [B, M, 4*(reg_max)] -> [B, M, 4, reg_max] -> [B, 4, M, reg_max]
-            pred_regs = pred_regs.reshape([B, M, 4, self.reg_max])
-            # [B, M, 4, reg_max] -> [B, reg_max, 4, M]
-            pred_regs = pred_regs.permute(0, 3, 2, 1).contiguous()
-            # [B, reg_max, 4, M] -> [B, 1, 4, M]
-            pred_regs = self.proj_conv(F.softmax(pred_regs, dim=1))
-            # [B, 1, 4, M] -> [B, 4, M] -> [B, M, 4]
-            pred_regs = pred_regs.view(B, 4, M).permute(0, 2, 1).contiguous()
-
         # tlbr -> xyxy
         pred_x1y1 = anchors - pred_regs[..., :2] * stride
         pred_x2y2 = anchors + pred_regs[..., 2:] * stride
@@ -151,9 +140,7 @@ class FreeYOLOv2(nn.Module):
         pyramid_feats = self.fpn(pyramid_feats)
 
         # non-shared heads
-        all_cls_preds = []
-        all_reg_preds = []
-        all_anchors = []
+        all_preds = []
         for level, (feat, head) in enumerate(zip(pyramid_feats, self.non_shared_heads)):
             cls_feat, reg_feat = head(feat)
 
@@ -173,41 +160,35 @@ class FreeYOLOv2(nn.Module):
             cls_pred = cls_pred.permute(0, 2, 3, 1).contiguous().view(B, -1, self.num_classes)
             reg_pred = reg_pred.permute(0, 2, 3, 1).contiguous().view(B, -1, 4*self.reg_max)
 
-            all_cls_preds.append(cls_pred)
-            all_reg_preds.append(reg_pred)
-            all_anchors.append(anchors)
-
-            B, M = cls_pred.shape[:2]
-            # no post process
-            cls_preds = torch.cat(all_cls_preds, dim=1)  # [B, M, C]
-            reg_preds = torch.cat(all_reg_preds, dim=1)  # [B, M, 4*(reg_max)]
-
             if self.use_dfl:
+                B, M = cls_pred.shape[:2]
                 # [B, M, 4*(reg_max)] -> [B, M, 4, reg_max] -> [B, 4, M, reg_max]
-                reg_preds = reg_preds.reshape([B, M, 4, self.reg_max])
+                reg_pred = reg_pred.reshape([B, M, 4, self.reg_max])
                 # [B, M, 4, reg_max] -> [B, reg_max, 4, M]
-                reg_preds = reg_preds.permute(0, 3, 2, 1).contiguous()
+                reg_pred = reg_pred.permute(0, 3, 2, 1).contiguous()
                 # [B, reg_max, 4, M] -> [B, 1, 4, M]
-                reg_preds = self.proj_conv(F.softmax(reg_preds, dim=1))
+                reg_pred = self.proj_conv(F.softmax(reg_pred, dim=1))
                 # [B, 1, 4, M] -> [B, 4, M] -> [B, M, 4]
-                reg_preds = reg_preds.view(B, 4, M).permute(0, 2, 1).contiguous()
+                reg_pred = reg_pred.view(B, 4, M).permute(0, 2, 1).contiguous()
+
+            # decode box
+            box_pred = self.decode_boxes(anchors, reg_pred, stride=self.stride[level])
 
             # [B, M, 4 + C]
-            preds = torch.cat([cls_preds.sigmoid(), reg_preds], dim=-1)
-            # NMS
-            outputs = non_max_suppression(
-                preds, self.conf_thresh, self.nms_thresh, classes=None, agnostic=False, max_det=self.max_det)
-            
-            print(outputs[0])
+            preds = torch.cat([cls_pred.sigmoid(), box_pred], dim=-1)
+            all_preds.append(preds)
 
-            return outputs
-
-        else:
-            # post process
-            bboxes, scores, labels = self.post_process(
-                all_cls_preds, all_reg_preds, all_anchors)
-            
-            return bboxes, scores, labels
+        # NMS
+        preds = torch.cat(all_preds, dim=1)
+        outputs = non_max_suppression(
+            preds, self.conf_thresh, self.nms_thresh, classes=None, agnostic=False, max_det=self.max_det)
+        
+        # Batch size = 1
+        bboxes = outputs[0][..., :4].float().cpu().numpy()
+        scores = outputs[0][..., 4].float().cpu().numpy()
+        labels = outputs[0][..., 5].long().cpu().numpy()
+        
+        return bboxes, scores, labels
 
 
     def forward(self, x):
