352a4422b2f08405623ea5debd3d1d9c26d47c4b
yjh0410 <1394571815@qq.com>
2023-03-23 09:03:33 +0800

    train FreeYOLOv2-Large on COCO

--- train.sh
+++ train.sh
@@ -4,7 +4,7 @@ python train.py \
         -d coco \
         --root /mnt/share/ssd2/dataset/ \
         -v yolo_free_v2_large \
-        -bs 16 \
+        -bs 14 \
         -size 640 \
         --wp_epoch 3 \
         --max_epoch 300 \
