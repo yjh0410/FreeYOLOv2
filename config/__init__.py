65bb96c9f2b5ebc40b69ef03164bbf062b5a8f76
yjh0410 <1394571815@qq.com>
2023-04-21 11:17:17 +0800

    modify backbone & pafpn

--- config/__init__.py
+++ config/__init__.py
@@ -4,7 +4,7 @@ from .yolo_free_v2_config import yolo_free_v2_cfg
 def build_config(args):
     print('==============================')
     print('Config: {} ...'.format(args.model.upper()))
-    if args.model in ['yolo_free_v2_nano', 'yolo_free_v2_tiny',
+    if args.model in ['yolo_free_v2_nano', 'yolo_free_v2_small', 'yolo_free_v2_medium',
                       'yolo_free_v2_large', 'yolo_free_v2_huge', # P5
                      ]:
         cfg = yolo_free_v2_cfg[args.model]
