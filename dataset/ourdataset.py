946eae03b36a7d234182d99ec157b82915669a63
yjh0410 <1394571815@qq.com>
2023-06-17 22:50:28 +0800

    debug ourdataset.py

--- dataset/ourdataset.py
+++ dataset/ourdataset.py
@@ -17,7 +17,7 @@ except:
     from transforms import yolov5_mosaic_augment, yolov5_mosaic_augment_9x, yolov5_mixup_augment
 
 # please define our class labels
-our_class_labels = ('butterfly', 'cat', 'dog', 'lion', 'person', 'tiger', )
+our_class_labels = ('bird', 'butterfly', 'cat', 'cow', 'dog', 'lion', 'person', 'pig', 'tiger', )
 
 
 
@@ -277,9 +277,11 @@ if __name__ == "__main__":
             color = class_colors[cls_id]
             # class name
             label = our_class_labels[cls_id]
-            image = cv2.rectangle(image, (int(x1), int(y1)), (int(x2), int(y2)), color, 2)
-            # put the test on the bbox
-            cv2.putText(image, label, (int(x1), int(y1 - 5)), 0, 0.5, color, 1, lineType=cv2.LINE_AA)
+            if x2 - x1 > 0. and y2 - y1 > 0.:
+                # draw bbox
+                image = cv2.rectangle(image, (int(x1), int(y1)), (int(x2), int(y2)), color, 2)
+                # put the test on the bbox
+                cv2.putText(image, label, (int(x1), int(y1 - 5)), 0, 0.5, color, 1, lineType=cv2.LINE_AA)
         cv2.imshow('gt', image)
         # cv2.imwrite(str(i)+'.jpg', img)
         cv2.waitKey(0)
